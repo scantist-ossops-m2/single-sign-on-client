fa8809f8587739ddcc6e67d61e4c28d7fef648ea
Nando <24811313+fzavalia@users.noreply.github.com>
2023-08-07 11:22:28 -0300

    fix: Add more comments

--- packages/lib/src/SingleSignOn.shared.ts
+++ packages/lib/src/SingleSignOn.shared.ts
@@ -1,14 +1,21 @@
 import { AuthIdentity } from "@dcl/crypto";
 
+// The possible typo of interactions with the iframe.
 export enum Action {
+  // Get the identity of a user.
   GET = "get",
+  // Store the identity of a user.
   STORE = "store",
+  // Clear the identity of a user.
   CLEAR = "clear",
+  // Check that the iframe can be communicated with.
   PING = "ping",
 }
 
+// Value used to identify messages intended for the iframe.
 export const SINGLE_SIGN_ON_TARGET = "single-sign-on";
 
+// The schema of messages sent by the client.
 export type ClientMessage = {
   target: string;
   id: number;
@@ -17,6 +24,7 @@ export type ClientMessage = {
   identity?: AuthIdentity | null;
 };
 
+// The schema of messages sent by the iframe.
 export type ServerMessage = {
   target: string;
   id: number;
@@ -24,6 +32,7 @@ export type ServerMessage = {
   error?: string;
 };
 
+// Helper function to create a message without the need of providing a target.
 export function createMessage(message: Omit<ClientMessage & ServerMessage, "target">) {
   return {
     target: SINGLE_SIGN_ON_TARGET,
@@ -31,26 +40,32 @@ export function createMessage(message: Omit<ClientMessage & ServerMessage, "targ
   };
 }
 
+// Get the identity of the user from local storage.
+// Does some extra operations on the obtained value to make it more reliable.
 export function localStorageGetIdentity(user: string) {
   const item = localStorage.getItem(getKey(user));
 
   const identity = item ? (JSON.parse(item) as AuthIdentity) : null;
 
   if (identity) {
+    // The expiration is parsed as a string, so we need to convert it to a Date.
     identity.expiration = new Date(identity.expiration);
   }
 
   return identity;
 }
 
+// Stores the identity of the user in local storage.
 export function localStorageStoreIdentity(user: string, identity: AuthIdentity) {
   localStorage.setItem(getKey(user), JSON.stringify(identity));
 }
 
+// Clears the identity of the user from local storage.
 export function localStorageClearIdentity(user: string) {
   localStorage.removeItem(getKey(user));
 }
 
+// Gets the key where the identity of the user is stored in local storage.
 function getKey(user: string) {
   if (!/^0x[a-fA-F0-9]{40}$/.test(user)) {
     throw new Error(`User must be a valid ethereum address`);
